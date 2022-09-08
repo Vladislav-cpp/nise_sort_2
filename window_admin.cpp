1329dee4a9b9e9d77b395731ba222c203669f801
Vladislav-cpp <105502335+Vladislav-cpp@users.noreply.github.com>
2022-09-08 13:34:43 +0300

    add window_check()

--- window_admin.cpp
+++ window_admin.cpp
@@ -1,6 +1,16 @@
 #include "window_admin.h"
 
 
+void window_admin::window_check()
+{
+	sf::Event event;
+	while (window.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+			window.close();
+	}
+}
+
 void window_admin::show(std::vector<int> elem)
 {
 
@@ -15,19 +25,14 @@ void window_admin::run()
 	while (window.isOpen())
 	{
 
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+
 
 		void bubble_sort(std::vector<int> elem);
 
 
 
-		window.clear();
-		//window.draw();
-		window.display();
+		////window.clear();
+		//////window.draw();
+		////window.display();
 	}
 }
--- window_admin.h
+++ window_admin.h
@@ -11,13 +11,18 @@ private:
 	sf::RenderWindow window;
 	base_sort_class& sort;
 
-	window_admin(base_sort_class& sort) :window(sf::VideoMode(500, 500), "SFMLworks"), sort(sort)
+    window_admin(base_sort_class& sort) :window(sf::VideoMode(500, 500), "SFMLworks"), sort(sort)
 	{
 
 
 	}
 
+	
+		
+
 public:
+
+	void window_check();
 
 	void show(std::vector<int> elem);
 
