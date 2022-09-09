a2135c6698e2229d298e42529424d08a61ab99ba
Vladislav-cpp <105502335+Vladislav-cpp@users.noreply.github.com>
2022-09-09 11:39:53 +0300

    add files

--- sort.cpp
+++ sort.cpp
@@ -31,9 +31,8 @@ void bubble_sort(std::vector<int> &elem)
 //    quick_sort(elem, elem.size()-1, 0);
 //}
 
-//void quick_sort(std::vector<int>& elem, int last , int first )
-
-void quick_sort_next(std::vector<int>& elem, int first, int last)
+void quick_sort_next(std::vector<int>& elem, int last , int first )
+//void quick_sort_next(std::vector<int>& elem, int first, int last)
 
 {
     int middle = elem.at((first + last) / 2);
